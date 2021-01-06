22ea4080cf971b08b5c7e229a1513002a05899f0
r0r0pr0n0s <romain.lambert2000@gmail.com>
2021-01-06 11:04:52 +0100

    test exo 1.3

--- js/exo1.js
+++ js/exo1.js
@@ -1,5 +1,9 @@
-gps = navigator.geolocation.getCurrentPosition(succesGPS);
+gps = navigator.geolocation.getCurrentPosition(succesGPS, errorGPS);
 
 function succesGPS(pos) {
     document.getElementById("test").innerHTML = "latitude : " + pos.coords.latitude + ", longitude : " + pos.coords.longitude;
+}
+
+function errorGPS(err) {
+    document.getElementById("test").innerHTML = "erreur";
 }
